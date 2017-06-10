09bbeb34f2ca79e764d59cb9912c4cc23d9f0b9f
SathishKumarParthasarathy <p.sathish.cs@gmail.com>
2017-06-10 11:26:31 -0500

    fix

--- src/main/java/org/hello/WordCountSpark.java
+++ src/main/java/org/hello/WordCountSpark.java
@@ -20,7 +20,7 @@ public class WordCountSpark {
 		Configuration fsConfig = new Configuration();
 		fsConfig.set("fs.defaultFS", "hdfs://hadoop.master.com:9000");
 		FileSystem fs = FileSystem.get(fsConfig);
-		Path outputPath = new Path("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Books/Science_Java");
+		Path outputPath = new Path("hdfs://hadoop.master.com:9000/user/psathishcs/Output/Books/Science_Java");
 		if (!fs.exists(outputPath)){
 			fs.delete(outputPath, true);
 		}
@@ -30,7 +30,7 @@ public class WordCountSpark {
 		JavaPairRDD<String, Integer> counts = words
 				.mapToPair(w -> new Tuple2<String, Integer>(w, 1))
 			    .reduceByKey((x, y) -> x + y);
-		counts.saveAsTextFile("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Books/Science_Java");
+		counts.saveAsTextFile("hdfs://hadoop.master.com:9000/user/psathishcs/Output/Books/Science_Java");
 		spark.close();
 	}
 }
