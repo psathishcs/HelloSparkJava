dbed3789f03bd8ef3bcbc8c7bf781a432ee62358
SathishKumarParthasarathy <p.sathish.cs@gmail.com>
2017-06-09 21:07:41 -0500

    first Spark RDD

---
+++ src/main/java/org/hello/HelloSpark.java
@@ -0,0 +1,21 @@
+package org.hello;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+/**
+ * 
+ * @author SathishParthasarathy
+ * Jun 9, 2017 8:52:55 PM
+ */
+public class HelloSpark {
+	public static void main(String[] args){
+		SparkConf conf = new SparkConf().setAppName("HelloSpark-Java");
+		JavaSparkContext spark = new JavaSparkContext(conf);
+		JavaRDD<String> textfile = spark.textFile("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Books/The_Outline_of_Science.txt");
+		long count =  textfile.count();
+		System.out.println("No of Lines -> " + count);
+
+	}
+}
