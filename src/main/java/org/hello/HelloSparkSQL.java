7b0121e8fa9546b2f8f6bdc4fb9854dde933f683
SathishKumarParthasarathy <p.sathish.cs@gmail.com>
2017-06-11 17:02:45 -0500

    adding selecting statment

--- src/main/java/org/hello/HelloSparkSQL.java
+++ src/main/java/org/hello/HelloSparkSQL.java
@@ -1,19 +1,23 @@
 package org.hello;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
 public class HelloSparkSQL {
 	public static void main(String[] args){
-		SparkConf conf = new SparkConf().setAppName("Hello Spark SQL - Java");
-		JavaSparkContext spark = new JavaSparkContext(conf);
-		SQLContext sqlcont = new SQLContext(spark);
-		Dataset<Row> df = sqlcont.read().json("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Json/Companies.json");
+		SparkSession spark = SparkSession.builder()
+										 .appName("HelloSparkSQL-Java")
+										 .config("spark.some.config.option", "some-value")
+										 .getOrCreate();
+		Dataset<Row> df = spark.read().json("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Json/Companies.json");
 		System.out.println("Companies Schema ------------------------------>");
 		df.printSchema();
+		System.out.println("Companies Schema ------------------------------>");
+		df.createOrReplaceTempView("Companies");
+		Dataset<Row> sqlDF = spark.sql("SELECT * FROM Companies Where _id.$oid = '52cdef7c4bab8bd675297d8a'");
+		sqlDF.show();
+
+		
 	}
 }
