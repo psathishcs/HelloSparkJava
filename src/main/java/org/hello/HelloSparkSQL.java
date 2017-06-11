d9480df819563b09b2fb6060705ffa0ea20ceb0b
SathishKumarParthasarathy <p.sathish.cs@gmail.com>
2017-06-11 17:10:06 -0500

    fix

--- src/main/java/org/hello/HelloSparkSQL.java
+++ src/main/java/org/hello/HelloSparkSQL.java
@@ -10,12 +10,12 @@ public class HelloSparkSQL {
 										 .appName("HelloSparkSQL-Java")
 										 .config("spark.some.config.option", "some-value")
 										 .getOrCreate();
-		Dataset<Row> df = spark.read().json("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Json/Companies.json");
+		Dataset<Row> df = spark.read().json("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Json/Companie.json");
 		System.out.println("Companies Schema ------------------------------>");
 		df.printSchema();
 		System.out.println("Companies Schema ------------------------------>");
-		df.createOrReplaceTempView("Companies");
-		Dataset<Row> sqlDF = spark.sql("SELECT * FROM Companies Where _id.$oid = '52cdef7c4bab8bd675297d8a'");
+		df.createOrReplaceTempView("Companie");
+		Dataset<Row> sqlDF = spark.sql("SELECT * FROM Companies");
 		sqlDF.show();
 
 		
